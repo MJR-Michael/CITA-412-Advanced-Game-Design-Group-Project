8eb2cd66fa3bfd24cb4cb88289272e35a98ad8df
JeremyRoalef <roalef944@morrisville.edu>
2025-09-30 13:49:43 -0400

    Update Edge Culling Behavior
    This commit readds the change for the proper edge culling behavior. The time spent fixing the main branch caused this to be removed. This commit reintroduces that.

--- Assets/Objects/Rooms/Scripts/ChamberMonoBehaviour.cs
+++ Assets/Objects/Rooms/Scripts/ChamberMonoBehaviour.cs
@@ -39,27 +39,6 @@ public class ChamberMonoBehaviour : MonoBehaviour
         }
     }
 
-    public void RenderEdges()
-    {
-        foreach (EdgeMonoBehaviour edgeMonoBehaviour in chamber.GetEdgeMonoBehaviours())
-        {
-            if (chamber.OriginGridPosition() == new GridPosition(25, 30))
-            {
-                Debug.Log("current edge being rendered: " + edgeMonoBehaviour.gameObject.name);
-            }
-            edgeMonoBehaviour.Render();
-        }
-    }
-
-    public void CullEdges(EdgeMonoBehaviour edgesNotToCull)
-    {
-        foreach (EdgeMonoBehaviour edge in chamber.GetEdgeMonoBehaviours())
-        {
-            if (edgesNotToCull == edge) continue;
-            edge.Cull();
-        }
-    }
-
     public void Initialize(Chamber chamber)
     {
         //Set chamber data
@@ -78,19 +57,6 @@ public class ChamberMonoBehaviour : MonoBehaviour
         foreach (Chamber adjacentChamber in chamber.GetConnectingChambers())
         {
             if (chambersNotToCullOut.Contains(adjacentChamber)) { continue; }
-
-            //cull out adjacent chamber's edges, but not the edge current is connecting to
-
-            //tell adjacent neighbors to the adjacent neighbor to cull out their edges
-            foreach (Chamber doublyAdjacentNeighbor in adjacentChamber.GetConnectingChambers())
-            {
-                if (doublyAdjacentNeighbor == chamber) continue; //No looping in on current chamber
-                EdgeMonoBehaviour edgeMonoBehaviourBetweenNeighborEdges = adjacentChamber.GetEdgeMonoBehaviourBetweenChambers(
-                    doublyAdjacentNeighbor);
-                if (edgeMonoBehaviourBetweenNeighborEdges == null) continue;
-
-                doublyAdjacentNeighbor.GetMonobehaviour().CullEdges(edgeMonoBehaviourBetweenNeighborEdges);
-            }
 
             //cull out the object
             adjacentChamber.GetMonobehaviour().Cull();
@@ -112,7 +78,6 @@ public class ChamberMonoBehaviour : MonoBehaviour
     {
         parentRenderer.SetActive(true);
         chamberTrigger.enabled = true;
-        RenderEdges();
         isRendered = true;
         foreach (EdgeMonoBehaviour edgeMonoBehaviour in chamber.GetEdgeMonoBehaviours())
         {
