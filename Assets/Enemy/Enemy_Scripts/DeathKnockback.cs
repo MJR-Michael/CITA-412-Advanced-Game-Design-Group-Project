6ff3006caf8bf4864a61ce143e87f8dd8fe029fc
Jandel <127899373+Jandel456@users.noreply.github.com>
2025-09-30 12:09:49 -0400

    DeathKnockback Update (again again)
    -Cleaned Up Old Scripts form the player folder
    -Preemptively updated DeathKnockback script to appeal to changes that will be made to the player movement script

--- Assets/Enemy/Enemy_Scripts/DeathKnockback.cs
+++ Assets/Enemy/Enemy_Scripts/DeathKnockback.cs
@@ -1,26 +1,23 @@
-using UnityEngine;
-
 public class DeathKnockback : MonoBehaviour, IDeathBehavior
 {
     [SerializeField] private float knockbackForce = 10f;
 
     public void OnDeath(Enemy enemy)
     {
-        //Checks to see if players are nearby
         Collider[] hits = Physics.OverlapSphere(enemy.transform.position, 5f);
         foreach (Collider hit in hits)
         {
             if (hit.CompareTag("Player"))
             {
-                Rigidbody rb = hit.GetComponent<Rigidbody>();
-                if (rb != null)
+                var controller = hit.GetComponent<StarterAssets.FirstPersonController>();
+                if (controller != null)
                 {
-                    //Pushes players
                     Vector3 direction = (hit.transform.position - enemy.transform.position).normalized;
-                    rb.AddForce(direction * knockbackForce, ForceMode.Impulse);
+                    controller.ApplyKnockback(direction * knockbackForce);
                 }
             }
         }
+
         Debug.Log("Knockback death triggered!");
         Destroy(gameObject, 1f);
     }
