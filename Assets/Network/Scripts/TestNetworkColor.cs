b771fc9a3ebfe67112260391a6408bfc91ba54f3
Michael <127899373+MJR-Michael@users.noreply.github.com>
2025-11-25 10:42:24 -0500

    Small Update to Testing Environment For Multiplayer

--- Assets/Network/Scripts/TestNetworkColor.cs
+++ Assets/Network/Scripts/TestNetworkColor.cs
@@ -5,12 +5,13 @@ public class TestNetworkColor : NetworkIdentity
 {
     [SerializeField] Color _color;
     [SerializeField] Renderer _renderer;
-
+    
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.A))
+        {
             SetColor(_color);
-
+        }
     }
 
     [ServerRpc]
@@ -18,11 +19,8 @@ public class TestNetworkColor : NetworkIdentity
     // if the color for client1 is yellow, and client 1 activates SetColor. Then the server will show yellow even if the server has Blue as their color.
     //This is because ServerRPC is client -> server instructions. and in our script we're taking the reference of whats taken in the serialized field of the client.
 
-
-    //  [ObserverRPC(bufferLast:true)]        this allows any new clients who have joined after an instruction was sent to the client, to still recieve those new instructions.
-    //  This is a simple level of syncronizing
-
-
+    //  [ObserverRPC(bufferLast:true)]        this allows any new clients who have joined after an instruction was sent to the client, to still receive those new instructions.
+    //  This is a simple level of synchronizing
     private void SetColor(Color color)
     {
         _renderer.material.color = color;
